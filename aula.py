be24f3fd2825d71ff615414404e81717359f2919
Gustavo Pietro de Assis Silva <gustavo.pas@puccampinas.edu.br>
2026-08-28 08:33:26 -0300

    segundo exemplo

--- aula.py
+++ aula.py
@@ -1,4 +1,12 @@
 #projeto 1
 
-nome = input("Digite seu nome:")
-print(f'Seu nome é {nome} .../n bom dia')
+#nome = input("Digite seu nome:")
+#print(f'Seu nome é {nome} .../n bom dia')
+
+#programa para aula de tec ti
+
+val = int(input("Digite seu numero de colunas:"))
+
+for i in range(val):
+  print("*", end="")
+print('')
